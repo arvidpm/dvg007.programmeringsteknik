a9ed202a1da37a0573f055b6e4f88edbe890a8e6
apm <amoosavi@kth.se>
2016-09-27 22:02:12 +0200

    added comments, minor adjustments

--- P-uppgift/Prototyp/Tennismatch-prototyp.py
+++ P-uppgift/Prototyp/Tennismatch-prototyp.py
@@ -1,20 +1,25 @@
 # # Programmeringsteknik webbkurs KTH inlämningsuppgift P-uppgift
-# Delmoment 1 - Kodskelett
-
-# Arvid Persson Moosavi
-# 2016-09-25
+# Delmoment 2 - Prototyp
 #
-# Detta är deluppgiften kodskelett huvuduppgiften Tennismatch
+# Arvid Persson Moosavi
+# 2016-09-27
+#
+# Detta är deluppgiften prototyp från huvuduppgiften Tennismatch
 #
 # Programmet fungerar i grova drag så att den läser indata från textfilen Spelarlista.txt
-# Existerar inte filen, eller om data från färra än två spelare finns, så kommer programmet avsluta
-# Sedan skapas spelarobjekt utifrån datan m.h.a. klassen Player och dess konstruktor.
+# I prototypuppgiften kollas inte filen, varken dess existens eller rimlig data.
+# Ingen felkontroll görs utöver att en spelare inte kan spela en match mot sig själv.
 #
-# Spelardatan laddas i main och skickas sedan som inparametrar till klassen Player
-# Klassen Match simulerar en match mellan två spelare, och innehåller en funktion för att uppdatera resultatlistan
+# Programflöde:
+# 1. main anropar createplayers med datafilen Spelarlista.txt.
+# 2. Data läses och spelarobjekt skapas utifrån antalet rader i Spelarlista.txt, 4 rader per spelare.
+# 3. Huvudmeny presenteras. Användaren väljer mellan att visa resultatlista/tillgängliga spelare, eller spela en match.
+# 4. Spelas en match väljer användaren två kombatanter, dessa får inte vara densamma.
+# 5. Matchen spelas, objekten uppdateras och ny data sparas till Spelarlista.txt
+# 6. Laddas resultatlistan på nytt finns ny data tillgänglig sedan förra matchen.
 #
 
-import sys, os.path
+import sys
 from itertools import islice
 from random import randint
 
@@ -45,53 +50,59 @@ def file_len(FILENAME):
 
 
 # En funktion som läser indata från FILENAME och sedan skapar objekt av Player
-def createplayers(FILENAME, FIRSTLINES):
+def createplayers(FILENAME, IGNORELINES):
 
     # Kontrollerar antalet rader i FILENAME
     num_lines = file_len(FILENAME)
 
-    player_count = int( (num_lines-FIRSTLINES) / 4)
+    player_count = int( (num_lines-IGNORELINES) / 4)
 
     p = 0
     s = 0
 
-    # Skapar array med all data från spelarna
+    # Skapar arrays för data. v1 för att hålla objekten och v2 för att hålla attributen
     v1 = [None] * player_count
     v2 = [None] * 4
 
 
     with open(FILENAME, encoding="utf-8") as file:
 
-        # Loopar igenom data från rad 5 till slutet
-        for line in islice(file, FIRSTLINES, num_lines):
+        # Loopar igenom data från rad 5 till slutet av textfilen
+        for line in islice(file, IGNORELINES, num_lines):
 
+            # Tar bort radbyte
             line = line.replace('\n', '')
 
             if p < 3:
 
+                # Varje rad sparas till element i v2
                 v2[p] = line
                 p += 1
 
             elif p == 3:
 
+                # Sista spelarraden sparas till elementet i v2
                 v2[p] = line
 
+                # Beräknar snitt vunna matcher
                 avg = round(( int(v2[2]) / int(v2[3]) ), 3)
 
+                # Spelarobjekt skapas från datan till v1
                 v1[s] = Player(v2[0], v2[1], v2[2], v2[3], avg)
 
+                # Nollställer p, ökar s för nästa objekt
                 p = 0
                 s += 1
 
+        # Returnerar lista med spelarobjekt
         return v1
 
 
 # En funktion som simulerar en match mellan två valda spelare
-def playmatch(player1, player2, FILENAME, FIRSTLINES, player):
+def playmatch(player1, player2, FILENAME, IGNORELINES, player):
 
     player1.played = int(player1.played)+1
     player2.played = int(player2.played)+1
-
 
     result = randint(0, 1)
 
@@ -107,20 +118,20 @@ def playmatch(player1, player2, FILENAME, FIRSTLINES, player):
 
     print("--------------------------------------------\n")
 
-    save(FILENAME, FIRSTLINES, player)
+    save(FILENAME, IGNORELINES, player)
 
 
-# En funktion som sparar ny data till Spelarlista.txt
-def save(FILENAME, FIRSTLINES, player):
+# En funktion som raderar gammal och sparar ny data till Spelarlista.txt
+def save(FILENAME, IGNORELINES, player):
+
     # Kontrollerar antalet rader i FILENAME
     num_lines = file_len(FILENAME)
 
-    # Raderar befintlig data, lämnar filen öppen
+    # Raderar befintlig data (totalt antal rader minus 5 första). Lämnar filen öppen
     oldlines = open(FILENAME).readlines()
-    open(FILENAME, 'w').writelines(oldlines[:-(num_lines - FIRSTLINES)])
+    open(FILENAME, 'w').writelines(oldlines[:-(num_lines - IGNORELINES)])
 
     # Skriver ny data
-
     for i in player:
         with open(FILENAME, "a", encoding='utf-8') as file:
             file.write(str(i.name) + '\n')
@@ -165,32 +176,35 @@ def print_playerlist(player):
 def main():
 
     # Första raderna i textfilen ignoreras
-    FIRSTLINES = 5
+    IGNORELINES = 5
 
     # Refererar textfil till konstant
     FILENAME = '../resources/Spelarlista.txt'
 
     # Skapar spelarobjekt från data i textfilen
-    player = createplayers(FILENAME, FIRSTLINES)
+    player = createplayers(FILENAME, IGNORELINES)
 
 
     # --------- Huvudmenyn -----------------------
-    choice = input("\nVälkommen!\n\n1. Spela match\n2. Visa spelare\n3. Visa resultat\n\n5. Avsluta\n\nGör ditt val: ")
+    choice = input("\nVälkommen till Tennisspelet!\n\n1. Spela match\n2. Visa spelare\n3. Visa resultat\n\n5. Avsluta\n\nGör ditt val: ")
 
     while choice:
 
         if choice[0] == "1":
+
             print_playerlist(player)
             print("\n")
 
             a = int(input("Välj spelare nr 1: "))
             b = int(input("Välj spelare nr 2: "))
+
             if a == b:
                 print("En spelare kan inte spela mot sig själv!")
+
             else:
                 print("\n"+player[a].name,"vs",player[b].name,"!\n")
                 print("--------------------------------------------\n")
-                playmatch(player[a], player[b], FILENAME, FIRSTLINES, player)
+                playmatch(player[a], player[b], FILENAME, IGNORELINES, player)
 
         elif choice[0] == "2":
             print_playerlist(player)
